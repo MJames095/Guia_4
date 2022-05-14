77c85bceacdb6a839f025d82ec7eab25e2c6d23c
MJames095 <james.mg095@gmail.com>
2022-05-14 16:08:48 -0600

    Primer commit

---
+++ app/src/main/java/sv/edu/udb/dsm/guia3/guia4/datos/Persona.java
@@ -0,0 +1,36 @@
+package sv.edu.udb.dsm.guia3.guia4.datos;
+
+public class Persona {
+    private String dui;
+    private String nombre;
+    String key;
+
+    public Persona() {
+    }
+
+    public Persona(String dui, String nombre) {
+        this.dui = dui;
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getKey() {
+        return key;
+    }
+    public void setKey(String key) {
+        this.key = key;
+    }
+    public String getDui() {
+        return dui;
+    }
+    public void setDui(String dui) {
+        this.dui = dui;
+    }
+}
+
+
